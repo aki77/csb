7a2acd5690bc16006211326ed329a30604239ba5
aki <lala.akira@gmail.com>
2020-05-14 09:37:06 +0900

    feat: Add ignore_class_names config

--- lib/csb/configuration.rb
+++ lib/csb/configuration.rb
@@ -1,10 +1,11 @@
 module Csb
   class Configuration
-    attr_accessor :utf8_bom, :streaming, :after_streaming_error
+    attr_accessor :utf8_bom, :streaming, :after_streaming_error, :ignore_class_names
 
     def initialize
       @utf8_bom = false
       @streaming = true
+      @ignore_class_names = %w[Puma::ConnectionError]
     end
   end
 end
